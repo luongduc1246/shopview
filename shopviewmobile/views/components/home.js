9ae77b10ca9793a9b66606744118e764510dd430
OfKiNgHeRo <luongduc1246@gmail.com>
2017-12-03 02:02:55 +0700

    làm mới lấy dữ liệu từ zing.vn

--- shopviewmobile/views/components/home.js
+++ shopviewmobile/views/components/home.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
-import {Text,View} from 'react-native';
-import Frisbee from 'frisbee';
+import {Text,View,AsyncStorage,FlatList,StyleSheet} from 'react-native';
 class Home extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
+    this.state={name:''};
   }
-  async getMoviesFromApi() {
+  load = async () => {
     try {
-      const api = new Frisbee({
-        baseURI: 'https://news.zing.vn',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
+      var b= await this.props.menu.then((value)=>{
+        return value;
       });
-      res = await api.get('', {
-
-    });
-      return res.body;
-    } catch(error) {
-      console.error(error);
+      this.setState({name:b});
+    } catch (e) {
+      console.error('Failed to load name.')
     }
   }
-  render() {
-    return (
-      <View style={{flex:1}} >
+  componentWillMount() {
+    this.load();
+    console.log('load');
+  }
+  componentWillUnmount() {
 
+  }
+  render() {
+    const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    row: {
+      padding: 15,
+      marginBottom: 5,
+      backgroundColor: 'skyblue',
+    },
+  });
+    var menu=[];
+    var i=0;
+    for(let item of this.state.name){
+      menu[i]={key:item};
+      i++;
+    }
+    return (
+      <View style={{flex:1}}>
+        <FlatList
+          style={styles.container}
+          data={menu}
+          renderItem={({item}) => <Text style={styles.row}>{item.key}</Text>}
+
+        />
       </View>
     );
   }
--- shopviewmobile/views/containers/homeContainer.js
+++ shopviewmobile/views/containers/homeContainer.js
@@ -1,11 +1,38 @@
 import { connect } from 'react-redux';
 import Home from './../components/home.js';
+import Frisbee from 'frisbee';
+import {AsyncStorage} from 'react-native';
+import cheerio from 'react-native-cheerio';
+import _ from 'lodash';
+async function getListMenu() {
+	try {
+		const api = new Frisbee({
+			baseURI: 'https://news.zing.vn',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		});
+		res = await api.get('',{
+
+	});
+	$=cheerio.load(res.body);
+	 let listMenu = new Set();
+	 $('li.parent a').each(function(i, elem){
+				listMenu.add($(this).text());
+	 });
+	return listMenu;
+	} catch(error) {
+		console.error(error);
+	}
+}
 function mapStateToProps(state) {
 		return {
+			menu:getListMenu(),
 			lang:state.language.lang
 		}
   }
- mapDispatchToProps = (dispatch, ownProps) => {
+mapDispatchToProps = (dispatch, ownProps) => {
   return {
 
   }
